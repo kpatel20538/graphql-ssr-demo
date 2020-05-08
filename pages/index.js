1b6c567870621f61eb6eb4af8fb489d54630a75b
Kishan Patel <kpatel16@tamu.edu>
2020-05-07 22:58:34 -0500

    Improved Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,77 @@
 import { query } from "../utils/api";
-import Box from "../components/Box";
-import Media from "../components/Media";
+import Link from "next/link";
 import PageLayout from "../components/PageLayout";
+import TypeTag from "../components/TypeTag";
+
+const PokemonCard = ({ id, name, classification, image, types }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <Link href={`/pokemon/${id}`}>
+            <a>
+              <img src={image} alt={name} />
+            </a>
+          </Link>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="columns is-centered is-mobile">
+          <div className="column has-text-centered">
+            <div className="title is-4">
+              <Link href={`/pokemon/${id}`}>
+                <a>{name}</a>
+              </Link>
+            </div>
+            <div className="subtitle is-6">{classification}</div>
+            <div>
+              <span className="tags">
+                {types.map((type) => (
+                  <TypeTag key={type} type={type} />
+                ))}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          figure img {
+            padding: 1em;
+            object-fit: contain;
+            object-position: center;
+          }
+          .tags {
+            justify-content: center;
+          }
+          .title,
+          .subtitle {
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
 const Home = ({ data }) => (
   <PageLayout>
-    <Box>
+    <div className="columns is-centered is-mobile is-multiline">
       {data.pokemons.map((pokemon) => (
-        <Media key={pokemon.id} {...pokemon} />
+        <div
+          key={pokemon.id}
+          className="column has-text-centered is-2-fullhd is-one-quarter-desktop is-one-third-tablet is-half-mobile"
+        >
+          <PokemonCard {...pokemon} />
+        </div>
       ))}
-    </Box>
+    </div>
   </PageLayout>
 );
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   return {
     props: await query({
       query: `
@@ -21,6 +79,7 @@ export async function getServerSideProps() {
           pokemons(first: 151) { 
             id
             name
+            classification
             image
             types
           }
